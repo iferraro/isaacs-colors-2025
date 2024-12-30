421a84b475e0905bf0c82fd105a5b171ae56a5a9
iferraro <ieferraro@outlook.com>
2024-12-29 21:49:45 -0800

    Basic table

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,25 +1,32 @@
 import { useSignal } from "@preact/signals";
 import Counter from "../islands/Counter.tsx";
+import isaacsColors from "../static/isaacs-custom-colors.json" with { type: "json" };
 
 export default function Home() {
   const count = useSignal(3);
   return (
-    <div class="px-4 py-8 mx-auto bg-[#86efac]">
+    <div class="px-4 py-8 mx-auto bg-black">
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-        <img
-          class="my-6"
-          src="/logo.svg"
-          width="128"
-          height="128"
-          alt="the Fresh logo: a sliced lemon dripping with juice"
-        />
-        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
-        <p class="my-4">
-          Try updating this message in the
-          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
-        </p>
-        <Counter count={count} />
+        <h1 class="text-4xl text-white font-bold">Isaacs Colors</h1>
+        <section>
+          <table>
+            <thead>
+              <tr class="text-white">
+                <th>Name</th>
+                <th>Color</th>
+              </tr>
+            </thead>
+            <tbody>
+              {isaacsColors.map((color) => (
+                <tr>
+                  <td class="text-white">{color.name}</td>
+                  <td style={{ backgroundColor: color.id }}></td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </section>
       </div>
-    </div>
+    </div>        
   );
 }

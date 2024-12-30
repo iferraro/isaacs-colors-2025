cd930a84d53743cc378cbbe122fc34a04e577f2d
iferraro <ieferraro@outlook.com>
2024-12-29 22:29:49 -0800

    Attempting clipboard functionality

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,13 +1,13 @@
 import { useSignal } from "@preact/signals";
 import Counter from "../islands/Counter.tsx";
 import isaacsColors from "../static/isaacs-custom-colors.json" with { type: "json" };
+import { copy } from "https://deno.land/x/clipboard/mod.ts";
 
 export default function Home() {
-  const count = useSignal(3);
   return (
     <div class="px-4 py-8 mx-auto bg-black">
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-        <h1 class="text-4xl text-white font-bold">Isaacs Colors</h1>
+        <h1 class="text-4xl text-white font-bold">Isaac&apos;s Colors</h1>
         <section>
           <table>
             <thead>
@@ -19,8 +19,13 @@ export default function Home() {
             <tbody>
               {isaacsColors.map((color) => (
                 <tr>
-                  <td class="text-white">{color.name}</td>
-                  <td style={{ backgroundColor: color.id }}></td>
+                  <td class="text-white">{color.name}</td>  
+                  <td>
+                    <button onClick={async () => { console.log("clicked"); await copy(color.id)}}>
+                      {/* May be good to have something like a checkmark to indicate that code was copied */}
+                      <div className="w-8 h-4" style={{ backgroundColor: color.id }} />
+                    </button>
+                  </td>
                 </tr>
               ))}
             </tbody>
@@ -30,3 +35,4 @@ export default function Home() {
     </div>        
   );
 }
+

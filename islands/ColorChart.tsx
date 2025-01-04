b5b7b63d36f7322bc53aa1d4eb75c0e43a4c09f5
iferraro <ieferraro@outlook.com>
2025-01-03 22:35:34 -0800

    Experimenting with box sizes

--- islands/ColorChart.tsx
+++ islands/ColorChart.tsx
@@ -1,5 +1,3 @@
-import { copy } from "https://deno.land/x/clipboard/mod.ts";
-
 type Color = {
   id: string;
   name: string;
@@ -17,15 +15,13 @@ function copyToClipboard(text: string) {
 
 function ColorChart({ colors }: ColorChartProps) {
   const colorChartBox = (color: Color) => (
-    <div class="flex flex-wrap border-2 border-black w-36 h-36">
-      <div class="flex-1 p-4 text-black">{color.name}</div>
-      <div class="flex-1 p-4">
+    <div class="flex flex-col border-2 border-white">
+      <div class="w-36 h-12 p-2 text-white">{color.name}</div>
+      <div class="w-36 h-24 p-2">
         <button
-          class="w-16 h-8 rounded-lg"
+          class="w-full h-full rounded-2xl"
           style={{
             backgroundColor: color.id,
-            borderWidth: 4,
-            borderColor: color.id,
           }}
           onClick={() => copyToClipboard(color.id)}
         />
@@ -34,7 +30,7 @@ function ColorChart({ colors }: ColorChartProps) {
   );
 
   return (
-    <section class="flex flex-wrap justify-center items-center gap-4">
+    <section class="flex flex-wrap justify-center items-center border-2 border-white">
       {colors.map((color) => colorChartBox(color))}
     </section>
   );
